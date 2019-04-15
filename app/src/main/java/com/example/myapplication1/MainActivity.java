8d0f1d36bbc11e14199ca2e6a40b13d8473f1cf1
digidudu06 <digidudu06@naver.com>
2019-04-15 19:05:16 +0900

    190415 19:05

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.myapplication1;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    int a, b ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,69 +19,84 @@ public class MainActivity extends AppCompatActivity {
 
         class Calc{
             int num, res;
-            String op;
-            public void execute(){
+
+            public void execute(String op){
                 switch (op){
-                    case "plus" :
+                    case "+" :
+                        res = a+b;
                         break;
-                    case "minu" :
+/*                    case "minu" :
+                        res = num-num;
                         break;
                     case "mul" :
+                        res = num*num;
                         break;
                     case "div" :
+                        res = num/num;
                         break;
+                    case "eq" :
+//                        (EditText)findViewById(R.id.num).setText;
+                        break;
+                    case "reset" :
+                        break;*/
                 }
             }
-            int getNum() {return num;}
-            void setNum() {this.num = num;}
+            int getNum() {return res;}
+            void setNum(int num) {this.num = num;}
         }
 
-        final EditText num2 = findViewById(R.id.num2);
-        final TextView result = findViewById(R.id.result);
+        final EditText num = findViewById(R.id.num);
+        final Calc c = new Calc();
 
         findViewById(R.id.plus).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int int2 = Integer.parseInt(num2.getText().toString());
-                Calc c = new Calc();
+                Toast.makeText(ctx, "버튼클릭", Toast.LENGTH_LONG).show();
+                a = Integer.parseInt(num.getText().toString());
+                num.setText("");
 
-                this.res = "";
+
+                findViewById(R.id.eq).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        b = Integer.parseInt(num.getText().toString());
+                        c.execute("+");
+                        num.setText(c.getNum()+"");
+                    }
+                });
+
             }
         });
         findViewById(R.id.minu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int int1 = Integer.parseInt(num1.getText().toString());
-                int int2 = Integer.parseInt(num2.getText().toString());
-                this.res = "";
+                int int1 = Integer.parseInt(num.getText().toString());
+                Calc c = new Calc();
+               // c.execute(v);
+
             }
         });
         findViewById(R.id.mul).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int int1 = Integer.parseInt(num1.getText().toString());
-                int int2 = Integer.parseInt(num2.getText().toString());
-                this.res = "";
+                int int1 = Integer.parseInt(num.getText().toString());
+                Calc c = new Calc();
+               // c.execute(v);
             }
         });
         findViewById(R.id.div).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int int1 = Integer.parseInt(num1.getText().toString());
-                int int2 = Integer.parseInt(num2.getText().toString());
-                this.res = "";
-            }
-        });
-
-        findViewById(R.id.eq).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                result.setText("");
+                int int1 = Integer.parseInt(num.getText().toString());
+                Calc c = new Calc();
+                //c.execute(v);
             }
         });
         findViewById(R.id.reset).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Calc c = new Calc();
+               // c.execute(v);
 
             }
         });
